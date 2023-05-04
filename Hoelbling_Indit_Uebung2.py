20415c7aa121d528ea5d55aded08c6aed858b838
Bigbrainbobby <132549385+Bigbrainbobby@users.noreply.github.com>
2023-05-04 11:19:47 +0200

    Add files via upload

---
+++ Hoelbling_Indit_Uebung2.py
@@ -0,0 +1,9 @@
+
+
+inta = int(input("Geben Sie hier Ihre Zahl ein: "))
+intb = int(input("Geben Sie hier Ihre zweite Zahl ein: "))
+
+if (intb == 0):
+        print("eine andere Zahl als 0, Dummkopf")
+else:
+        print("Ihre Summe ist: ",inta + intb)
