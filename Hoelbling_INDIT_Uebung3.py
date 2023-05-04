3ee97886a33ab6d1edf2f8a5037f4656f5a37243
Bigbrainbobby <132549385+Bigbrainbobby@users.noreply.github.com>
2023-05-04 11:38:44 +0200

    Add files via upload

---
+++ Hoelbling_INDIT_Uebung3.py
@@ -0,0 +1,13 @@
+x=0
+
+y=int(input("Geben Sie hier eine Zahl ein: "))
+
+while y==0:
+        
+    print("Eine andere Zahl als 0")
+    y = int(input("Geben Sie hier eine Zahl ein: "))
+
+while x<=y:
+    print(x)
+    x+=1
+    
